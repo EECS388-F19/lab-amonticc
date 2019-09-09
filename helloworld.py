5699732b12cf367d336b86eb127e963d18fc6806
amonticc <amonticc@aol.com>
2019-09-09 17:45:29 -0400

    Pushing Python code

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+from random import randint;
+x = randint(1,100);
+y = randint(1,100);
+z = x + y;
+print ("Alexander Monticciolo");
+print (x);
+print (y);
+print ("Sum = " + str(z));
+print ("Average = " + str(z/2.0));
